615e4788145fae7745514071cc9963806028ea21
YOSEPH KIM <87624455+SepSeph@users.noreply.github.com>
2021-08-11 18:13:15 -0700

    Intro

---
+++ Login/External JS for html practice.js
@@ -0,0 +1,9 @@
+
+    function login () {
+        let id = $('#input-id').val()
+        if (id == '') {
+            alert("다시 입력해주세요")
+        } else {
+            alert(id + "님 헤브론 교회 청년부 사이트에 오신 것을 환영합니다.")
+        }
+    }
