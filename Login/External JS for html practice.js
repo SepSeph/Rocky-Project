7a223b4fe54b9e403bd8baf874bcb6864c44e493
YOSEPH KIM <87624455+SepSeph@users.noreply.github.com>
2021-08-12 16:29:51 -0700

    attendence

--- Login/External JS for html practice.js
+++ Login/External JS for html practice.js
@@ -7,3 +7,11 @@
             alert(id + "님 헤브론 교회 청년부 사이트에 오신 것을 환영합니다.")
         }
     }
+
+    function name () {
+        let id = $('#input-name').val();
+        let temp_html = `<li>${id}</li>`
+
+        $('attend-name').append(temp_html)
+
+    }
